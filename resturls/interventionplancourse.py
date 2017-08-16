a46daad1f65efd1875da1c8f3c3390e444727504
frederic.catellier <frederic.catellier@cauca.ca>
2017-08-16 07:43:38 -0400

    feat(interventionplancourse url): add lanes to the intervention plan course result.

--- resturls/interventionplancourse.py
+++ resturls/interventionplancourse.py
@@ -2,7 +2,10 @@ import uuid
 
 from cause.api.management.core.database import Database
 from cause.api.management.resturls.base import Base
+from cause.api.survip.models.lane import Lane
+from cause.api.survip.resturls.mappers.lane_light_mapper import LaneLightMapper
 from ..models.intervention_plan_course import InterventionPlanCourse as Table
+from ..models.intervention_plan_course_lane import InterventionPlanCourseLane as TableLane
 
 
 class InterventionPlanCourse(Base):
@@ -15,11 +18,37 @@ class InterventionPlanCourse(Base):
 		'PATCH': '',
 	}
 
+	def get_orientation_description(self, row):
+		if row[2] == 1:
+			return " (gauche)"
+		elif row[2] == 2:
+			return " (droite)"
+		else:
+			return ""
+
+	def get_lanes(self, db, id_intervention_plan_course):
+		data = db. \
+			query(TableLane.id_intervention_plan_course_lane, TableLane.sequence, TableLane.direction, Lane). \
+			join(Lane, Lane.id_lane == TableLane.id_lane). \
+			filter(TableLane.is_active == '1', TableLane.id_intervention_plan_course == id_intervention_plan_course). \
+			order_by(TableLane.sequence). \
+			all()
+
+		result = [{'id_intervention_plan_course_lane': r[0], 'sequence': r[1], 'description': LaneLightMapper.generate_full_name(r[3], 'fr') + self.get_orientation_description(r)} for r in data]
+
+		return result
+
 	def get(self, id_intervention_plan_course):
 		with Database() as db:
 			data = db.query(Table).filter(Table.is_active == '1', Table.id_intervention_plan_course == id_intervention_plan_course).all()
+			lanes = self.get_lanes(db, id_intervention_plan_course)
 
-		return {'data': data}
+		return {
+			'data': {
+				'course': data,
+				'lanes': lanes
+			}
+		}
 
 	def create(self, args):
 		if 'id_firestation' not in args or 'id_intervention_plan' not in args:
