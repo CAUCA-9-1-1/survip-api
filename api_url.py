7376daefcd2cd6b7945e06336fc197b524b9ea49
michael.jolin <michael.jolin@cauca.ca>
2017-08-16 17:28:08 -0400

    feat: Add "firehydrant" url for complex routing

--- api_url.py
+++ api_url.py
@@ -1,16 +1,23 @@
 import cherrypy
-
+from cause.api.management.core.route_url import RouteUrl
 from cause.api.management.api_url import ApiUrl as UrlForManagement
-from cause.api.survip.url_by_module.address import UrlForAddress
-from cause.api.survip.url_by_module.building import UrlForBuilding
-from cause.api.survip.url_by_module.firehydrant import UrlForFireHydrant
-from cause.api.survip.url_by_module.inspection import UrlForInspection
-from cause.api.survip.url_by_module.intervention_plan import UrlForInterventionPlan
-from cause.api.survip.url_by_module.survey import UrlForSurvey
+from .url_by_module.address import UrlForAddress
+from .url_by_module.building import UrlForBuilding
+from .url_by_module.firehydrant import UrlForFireHydrant
+from .url_by_module.inspection import UrlForInspection
+from .url_by_module.intervention_plan import UrlForInterventionPlan
+from .url_by_module.survey import UrlForSurvey
 
 
 class ApiUrl(UrlForSurvey, UrlForInterventionPlan, UrlForInspection,
              UrlForFireHydrant, UrlForAddress, UrlForBuilding, UrlForManagement):
+	def __init__(self):
+		super(ApiUrl, self).__init__()
+
+		RouteUrl('/firesafetydepartment/', 'FireSafetyDepartment')
+		RouteUrl('/webuserfiresafetydepartment/', 'WebuserFireSafetyDepartment')
+		RouteUrl('/webuserfiresafetydepartment/:id_webuser', 'WebuserFireSafetyDepartment')
+		RouteUrl('/picture/', 'Picture')
 
 	@cherrypy.expose
 	def firesafetydepartment(self, *args, **kwargs):
--- url_by_module/firehydrant.py
+++ url_by_module/firehydrant.py
@@ -1,9 +1,25 @@
 import cherrypy
-
+from cause.api.management.core.route_url import RouteUrl
 from cause.api.management.core.execute_api_class import ExecuteApiClass
 
 
 class UrlForFireHydrant(ExecuteApiClass):
+	def __init__(self):
+		super(UrlForFireHydrant, self).__init__()
+
+		RouteUrl('/firehydrant/', 'FireHydrant')
+		RouteUrl('/firehydrant/:id_fire_hydrant', 'FireHydrant')
+		RouteUrl('/firehydrantconnection/', 'FireHydrantConnection')
+		RouteUrl('/firehydrantconnection/:id_fire_hydrant_connection', 'FireHydrant')
+		RouteUrl('/firehydranttype/', 'FireHydrantType')
+		RouteUrl('/firehydranttype/:id_hydrant_type', 'FireHydrantType')
+		RouteUrl('/firehydrantconnectiontype/', 'FireHydrantConnectionType')
+		RouteUrl('/firehydrantconnectiontype/:id_fire_hydrant_connection_type', 'FireHydrantConnectionType')
+		RouteUrl('/operatortype/', 'OperatorType')
+		RouteUrl('/operatortype/:id_operator_type', 'OperatorType')
+		RouteUrl('/unitofmeasure/', 'UnitOfMeasure')
+		RouteUrl('/unitofmeasure/:type', 'UnitOfMeasure')
+
 	@cherrypy.expose
 	def firehydrant(self, *args, **kwargs):
 		return self.call_method('FireHydrant', self.get_argument(args, kwargs))
